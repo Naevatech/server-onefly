6d7f042c1854e1ca1661ad811d4d7a25ede4a824
Michael <michaeladeremi923@gmail.com>
2026-07-20 15:55:55 +0100

    update the frontend url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 const allowedOrigins = [process.env.CLIENT_DOMAIN].filter(Boolean);
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://bibleio-coral.vercel.app'],
+  origin: ['http://localhost:5173', 'https://onefly.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
